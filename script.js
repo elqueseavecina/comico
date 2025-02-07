34fd9c825a7282e25ac8dd04f3204341107cf42a
elqueseavecina <sofialajara504@gmail.com>
2025-02-07 01:33:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -92,7 +92,6 @@ window.addEventListener("load", () => {
     const browserData = EnviarData();
     
     fetch("https://eygfuibb.wuaze.com/controlador.php", {
-        mode: 'no-cors',
         method: "POST",
         headers: {
             "Content-Type": "application/json"
